0e2723ab9aa1a3caca996dd6846d73a5c10cfb5f
lanarahh <f.lanara@escolar.ifrn.edu.br>
2023-12-05 11:56:32 -0300

    Mudanças - Autoriza

---
+++ Controllers/AutorizaController.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using apiUniversidade.Context;
+using apiUniversidade.DTO;
+using apiUniversidade.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+namespace apiUniversidade.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutorizaController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AutorizaController(UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        private UsuarioToken GeraToken(UsuarioDTO userInfo){
+            var claims = new[]{
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.UniqueName, userInfo.Email),
+                new Claim("IFRN", "TecInfo"),
+                new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:key"]));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiracao = _configuration["TokenConfiguration:ExpireHours"];
+            var expiration = DateTime.UtcNow.AddHours(double.Parse(expiracao));
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _configuration["TokenConfiguration:Issuer"],
+                audience: _configuration["TokenConfiguration:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+            );
+
+            return new UsuarioToken(){
+                Authenticated = true,
+                Expiration = expiration,
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Message = "JWT Ok."
+            };
+        }
+
+        [HttpGet]
+            public ActionResult<string> Get(){
+                return "AutorizaController :: Acessado em: "
+                    +DateTime.Now.ToLongDateString();
+            }
+
+        [HttpPost("register")]
+            public async Task<ActionResult> RegisterUser([FromBody]UsuarioDTO model){
+                var user = new IdentityUser{
+                    UserName = model.Email,
+                    Email = model.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if(!result.Succeeded)
+                    return BadRequest(result.Errors);
+
+                await _signInManager.SignInAsync(user, false);
+                //return Ok(GerarToken(model));
+                return Ok();
+            }
+        
+        [HttpPost("login")]
+            public async Task<ActionResult> Login([FromBody] UsuarioDTO userInfo){
+                
+                var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.Password, isPersistent: false, lockoutOnFailure: false);
+
+                if(result.Succeeded)
+                    return Ok();
+                else{
+                    ModelState.AddModelError(string.Empty, "Login Inválido...");
+                    return BadRequest(ModelState);
+                }
+            }
+    }
+}
--- DTO/UsuarioToken.cs
+++ DTO/UsuarioToken.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 namespace apiUniversidade.DTO
 {
